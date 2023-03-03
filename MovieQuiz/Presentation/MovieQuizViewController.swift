8d4460114a7ec9ac72a85d044209f43c905e4364
bkljn4g <bkljn4g@gmail.com>
2023-03-03 15:29:46 +0300

    Поправлена верстка лейбла для iPhone 12 mini, фон вью соответствует макету.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,28 +1,28 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
+    /// MARK: - Lifecycle
     
-    //структура вопросов
+    ///структура вопросов
     struct QuizQuestion {
         let image: String
         let text: String
         let correctAnswer: Bool
     }
     
-    //состояние "Вопрос задан":
+    ///состояние "Вопрос задан":
     struct QuizStepViewModel {
         let image: UIImage
         let question: String
         let questionNumber: String
     }
     
-    //состояние "Результат ответа":
+    ///состояние "Результат ответа":
     struct QuizQuestionResultsViewModel {
         let answer: Bool
     }
     
-    //состояние "Результат квиза":
+    ///состояние "Результат квиза":
     struct QuizResultsViewModel {
         let title: String
         let text: String
@@ -30,7 +30,7 @@ final class MovieQuizViewController: UIViewController {
     }
     
     
-    //добавляем переменные отображения элементов
+    ///добавляем переменные отображения элементов
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
@@ -38,13 +38,13 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var noButton: UIButton!
     
     
-    //переменная индекс текущего вопроса
+    ///переменная индекс текущего вопроса
     private var currentQuestionIndex: Int = 0
-    //переменная индекс количества правильных ответов
+    ///переменная индекс количества правильных ответов
     private var correctAnswers: Int = 0
     
     
-    //переменная вопросы, содержит массив с данными
+    ///переменная вопросы, содержит массив с данными
     private let questions: [QuizQuestion] = [
         QuizQuestion(image: "The Godfather",
                      text: "Рейтинг этого фильма больше чем 6?",
@@ -79,7 +79,7 @@ final class MovieQuizViewController: UIViewController {
     ]
     
     
-    //функция переопределения. Добавляем константу вью модели
+    ///функция переопределения. Добавляем константу вью модели
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -88,7 +88,7 @@ final class MovieQuizViewController: UIViewController {
     }
     
     
-    //в этой функции заполняем картинку, текст и счетчик данными
+    ///в этой функции заполняем картинку, текст и счетчик данными
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
@@ -100,7 +100,7 @@ final class MovieQuizViewController: UIViewController {
     
     
     
-    //делаем рамку картинки красной или зеленой в зависимости от ответа
+    ///делаем рамку картинки красной или зеленой в зависимости от ответа
     private func showAnswerResult(isCorrect: Bool) {
         yesButton.isEnabled = false
         noButton.isEnabled = false
@@ -108,31 +108,31 @@ final class MovieQuizViewController: UIViewController {
             correctAnswers += 1
         }
         
-        imageView.layer.masksToBounds = true // отрисовка рамки
-        imageView.layer.borderWidth = 8 // толщина рамки
+        imageView.layer.masksToBounds = true /// отрисовка рамки
+        imageView.layer.borderWidth = 8 /// толщина рамки
         imageView.layer.cornerRadius = 20
-        imageView.layer.borderColor = isCorrect ? UIColor.green.cgColor : UIColor.red.cgColor //проверка Да или Нет
+        imageView.layer.borderColor = isCorrect ? UIColor.green.cgColor : UIColor.red.cgColor ///проверка Да или Нет
         
-        //запуск задачи через 1 секунду
+        ///запуск задачи через 1 секунду
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.yesButton.isEnabled = true
             self.noButton.isEnabled = true
             self.showNextQuestionOrResults()
-            self.imageView.layer.borderWidth = 0 //после перехода на следующий вопрос убираем рамку
+            self.imageView.layer.borderWidth = 0 ///после перехода на следующий вопрос убираем рамку
         }
     }
     
     
-    //функция конвертации
+    ///функция конвертации
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(
-            image: UIImage(named: model.image) ?? UIImage(), //распаковка картинки
-            question: model.text, //берется текст вопроса
+            image: UIImage(named: model.image) ?? UIImage(), ///распаковка картинки
+            question: model.text, ///берется текст вопроса
             questionNumber: "\(currentQuestionIndex + 1)/ \(questions.count)"
         )
     }
     
-    //показываем вопрос, или результаты всего квиза
+    ///показываем вопрос, или результаты всего квиза
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
             let text = "Ваш результат: \(correctAnswers) из 10"
@@ -151,7 +151,7 @@ final class MovieQuizViewController: UIViewController {
     }
     
     
-    //в этой функции показываем результат прохождения квиза
+    ///в этой функции показываем результат прохождения квиза
     private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
             title: result.title,
@@ -167,10 +167,10 @@ final class MovieQuizViewController: UIViewController {
                 
                 self.currentQuestionIndex = 0
                 
-                //сброс счетчика ответов
+                ///сброс счетчика ответов
                 self.correctAnswers = 0
                 
-                //заново показываю первый вопрос
+                ///заново показываю первый вопрос
                 let firstQuestion = self.questions[self.currentQuestionIndex]
                 let viewModel = self.convert(model: firstQuestion)
                 self.show(quiz: viewModel)
@@ -181,14 +181,14 @@ final class MovieQuizViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    //экшн для кнопки Да
+    ///экшн для кнопки Да
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
-    //экшн для кнопки Нет
+    ///экшн для кнопки Нет
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
