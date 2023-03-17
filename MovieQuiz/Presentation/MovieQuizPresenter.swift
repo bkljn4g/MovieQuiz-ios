4c1c417ba633fed3703f7dac3fd3d0ff5053e1ca
bkljn4g <bkljn4g@gmail.com>
2023-03-17 21:55:49 +0300

    Промежуточное сохранение

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -39,7 +39,7 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
         }
     
     
-    private let questionsAmount: Int = 10
+        private let questionsAmount: Int = 10
         private var currentQuestionIndex: Int = 0
         private var correctAnswers: Int = 0
         
@@ -57,16 +57,20 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
         func yesButtonClicked() {
             didAnswer(isYes: true)
         }
+    
+    
         func noButtonClicked() {
             didAnswer(isYes: false)
         }
         
+    
         private func didAnswer(isCorrect: Bool) {
             if isCorrect {
                 correctAnswers += 1
             }
         }
         
+    
         private func didAnswer(isYes: Bool) {
             guard let currentQuestion = currentQuestion else {
                 return
@@ -77,6 +81,7 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
             showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
         }
         
+    
         private func isLastQuestion() -> Bool {
             currentQuestionIndex == questionsAmount - 1
         }
@@ -85,12 +90,15 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
         func switchToNextQuestion() {
             currentQuestionIndex += 1
         }
+    
+    
         func restartGame() {
             currentQuestionIndex = 0
             correctAnswers = 0
             questionFactory?.requestNextQuestion()
         }
         
+    
         func convert(model: QuizQuestion) -> QuizStepViewModel {
             QuizStepViewModel(
                 image: UIImage(data: model.image) ?? UIImage(),
@@ -99,7 +107,7 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
         }
         
         private func showNextQuestionOrResults() {
-            if isLastQuestion(){
+            if isLastQuestion() {
                 guard let statisticService = statisticService else { return }
                 statisticService.store(correct: correctAnswers, total: questionsAmount)
                 let totalAccuracyPercentage = (String(format: "%.2f", statisticService.totalAccuracy) + "%")
