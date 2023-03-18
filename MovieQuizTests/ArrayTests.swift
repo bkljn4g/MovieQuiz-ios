1d4b4acd62907b5dbe4cf3515f1f53291a277420
bkljn4g <bkljn4g@gmail.com>
2023-03-18 13:40:15 +0300

    fix ArrayTests

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -13,7 +13,7 @@ import XCTest
 class ArrayTests: XCTestCase {
     func testGetValueInRange() throws { //тест на успешное взятие элемента по индексу
         // Given
-        let array = [1, 1, 2, 3, 5]
+        let array = [1, 4, 2, 3, 5]
         
         // When
         let value = array[safe: 2]
@@ -25,7 +25,7 @@ class ArrayTests: XCTestCase {
     
     func testGetValueOutOfRange() throws { // тест на взятие элемента по неправильному индексу
         // Given
-        let array = [1, 1, 2, 3, 5]
+        let array = [1, 4, 2, 3, 5]
         
         // When
         let value = array[safe: 20]
